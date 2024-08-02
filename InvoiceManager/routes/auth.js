ce537977f8b07349f61679b72e9a6444b320f3e0
ramonyd <118173456+ramonyd@users.noreply.github.com>
2024-08-02 15:47:47 +0200

    added user functionality

--- InvoiceManager/routes/auth.js
+++ InvoiceManager/routes/auth.js
@@ -45,7 +45,7 @@ passport.use(
 
 passport.serializeUser(function (user, cb) {
   process.nextTick(function () {
-    cb(null, { id: user.id, username: user.username });
+    cb(null, { id: user.id, username: user.username, isAdmin: user.isAdmin == 1 });
   });
 });
 
--- InvoiceManager/routes/users.js
+++ InvoiceManager/routes/users.js
@@ -4,8 +4,31 @@ const db = require('./../models')
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  const users = await db.users.findAll({});
-  res.render('users', users);
+  const users = await db.users.findAll({ raw: true});
+  console.log(users);
+  res.render('users', { users });
+});
+
+/* GET user by id. */
+router.get('/:id', async function(req, res, next) {
+  const user = await db.users.findByPk(req.params.id, { raw: true });
+  res.render('user', { ...user, isAdmin: user.isAdmin == 1, disableInputs: req.user.isAdmin == 0 });
+});
+
+/* POST user by id. */
+router.post('/:id', async function(req, res, next) { 
+  const body = req.body;
+
+  const user = await db.users.findByPk(req.params.id, { raw: true });
+  if(!user) 
+    return res.status(404).send('User not found');
+
+  user.name = body.name;
+  user.email = body.email;
+  user.isAdmin = (body.isAdmin == 'on');
+
+  db.users.update(user, { where: { id: req.params.id }});
+  return res.redirect('/users');
 });
 
 module.exports = router;
